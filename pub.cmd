94b82382562b6c7ac245e973fd2d1767dadfbcd5
John M. Wargo <30843404+jwargo@users.noreply.github.com>
2017-09-27 15:21:56 -0400

    editing .gitignore

--- pub.cmd
+++
@@ -1,4 +0,0 @@
-git add -A :/
-git commit -m "updated %date% %time%"
-git push
-start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://github.com/johnwargo/ncdevcon-2017
--- pub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-updateStr="Updated "
-updateStr+=$(date)
-updateStr+=" "
-updateStr+=$(time)
-git add -A :/
-git commit -m "${updateStr}"
-git push
